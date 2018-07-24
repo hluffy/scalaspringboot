a29251868fbf86b1fe93badd5053aad354adebb2
hluffy <18801613171@163.com>
2018-07-25 02:00:44 +0800

    error

--- src/main/java/com/dk/springboot/RunApp.scala
+++ src/main/java/com/dk/springboot/RunApp.scala
@@ -1,8 +1,8 @@
 package com.dk.springboot
 
 import org.springframework.boot.SpringApplication
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
+
 
 @SpringBootApplication
 class RunApp {
